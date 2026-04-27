0d99836b828105b7263cf7eaaa40750957029666
rahulye <rahulmykris@gmail.com>
2026-04-27 18:49:26 +0530

    chore: add equality cases for JWT cookies

--- todo-fullstack/backend/src/utils/generateToken.js
+++ todo-fullstack/backend/src/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (userId, res) => {
 	res.cookie("jwt", token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
-		sameSite: "none",
+		sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
 		maxAge: 1000 * 60 * 60 * 24 * 1,
 	});
 
